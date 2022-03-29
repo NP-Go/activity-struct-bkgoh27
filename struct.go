97f29ba7284815742f2d9fd1d1bfe48cc3083c9d
bkgoh27 <bengkoon.goh27@gmail.com>
2022-03-29 17:17:41 +0800

    play with struct

--- struct.go
+++ struct.go
@@ -1,7 +1,46 @@
 package main
 
-//Insert struct declaration here
+import "fmt"
+
+// declaration of struct
+type customer struct {
+	fName            string
+	lName            string
+	age              int
+	subscriber       bool
+	homeAddress      string
+	phone            int
+	creditAvailable  float32
+	currentCartCost  float32
+	currentOrderCost float32
+}
 
 func main() {
-	//Insert code here
+	// create the object
+	customer1 := customer{
+		fName:            "Annakin",
+		lName:            "Skywalker",
+		age:              50,
+		subscriber:       true,
+		homeAddress:      "Death Star",
+		phone:            1234567,
+		creditAvailable:  10000.00,
+		currentCartCost:  0.00,
+		currentOrderCost: 0.00,
+	}
+
+	customer2 := customer{
+		fName:            "Annakin2",
+		lName:            "Skywalker2",
+		age:              502,
+		subscriber:       true,
+		homeAddress:      "Death Star2",
+		phone:            1234567,
+		creditAvailable:  10000.00,
+		currentCartCost:  0.00,
+		currentOrderCost: 0.00,
+	}
+
+	// customerbase := []customer{customer1, customer2}
+	fmt.Println(customer1.fName, customer2.lName)
 }
